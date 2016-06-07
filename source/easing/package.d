c918bd03da9ad6b09544f270ee951ae7396cf2ff
tanitta <ttata.trit@gmail.com>
2016-06-08 04:06:22 +0900

    Enable to accept optional args to map.

--- source/easing/package.d
+++ source/easing/package.d
@@ -2,14 +2,31 @@ module easing;
 
 import easing.functions;
 
-pure T map(alias F = linear, T, Options ...)(in T v, in T minIn, in T maxIn, in T minOut, in T maxOut, Options options){
+pure T map(alias F = linear, T, Options...)( in T v, in T minIn, in T maxIn, in T minOut, in T maxOut, Options options){
 	T time = v - minIn;
 	T change = maxOut - minOut;
 	T duration = maxIn - minIn;
 	T begin = minOut;
 	
-	return F(time, begin, duration, change, options);
+	static if(Options.length){
+		return F(time, begin, duration, change, options);
+	}else{
+		return F(time, begin, duration, change);
+	}
+	
 }
+
+//map should accept optional args
+version(unittest){
+	T multiArgsFunction(T)( T time,  T begin,  T duration,  T change, T option1, T option2){return option2;}
+}
+unittest{
+	assert(__traits(compiles, {
+		0.0.map!multiArgsFunction(0.0, 10.0, 0.0, 1.0, 0.5, 2.0);
+	}));
+	assert(0.0.map!multiArgsFunction(0.0, 10.0, 0.0, 1.0, 0.5, 2.0) == 2.0);
+}
+
 unittest{
 	assert(0.0.map!linear(0.0, 10.0, 0.0, 1.0) == 0.0);
 	assert(5.0.map!linear(0.0, 10.0, 0.0, 1.0) == 0.5);
@@ -20,6 +37,7 @@ unittest{
 	assert(10.0.map!(Quad.easeIn)(0.0, 10.0, 0.0, 1.0) == 1.0);
 	
 	assert(0.0.map!easeInQuad(0.0, 10.0, 0.0, 1.0) == 0.0.map!(Quad.easeIn)(0.0, 10.0, 0.0, 1.0));
+	
 }
 
 pure T clamp(T)(in T v, in T min, in T max){
